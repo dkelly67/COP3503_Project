68b7f05317d7d7747a82a5e7f9cc41af02a86b2c
william-benfer <j.benfer@ufl.edu>
2014-04-13 22:39:33 -0400

    Fraction and completed Exponent

---
+++ Exponent.cpp
@@ -0,0 +1,205 @@
+/*
+ * Exponent.cpp
+ *
+ *  Created on: Apr 13, 2014
+ *      Author: Jackson
+ */
+
+#include "Exponent.h"
+#include <sstream>
+#include <typeinfo>
+#include "Integer.h"
+#include "Fraction.h"
+#include "Constant.h"
+#include <iostream>
+#include "Multiplication.h"
+#include "Root.h"
+#include "Summation.h"
+#include <math.h>
+
+using namespace std;
+
+Exponent::Exponent(Number* inside, Number* power){
+	this->inside = inside;
+	this->power = power;
+}
+
+Number* Exponent::calculate(){
+
+
+	inside = inside->calculate();
+	power = power->calculate();
+
+	if(typeid(*inside) == typeid(Integer) && typeid(*power) == typeid(Integer)){
+		Number* ans = new Integer(this->getDecimal());
+		delete this;
+		return ans;
+	}
+
+	if(typeid(*power) == typeid(Integer)){
+
+		Integer* pow = (Integer*) power;
+		int p = pow->getInteger();
+
+		if(p == 0){
+			delete this;
+			return new Integer(1);
+		}
+		if(p == 1)
+			return inside;
+
+		if(p< 0){
+			Fraction* f = new Fraction(new Integer(1), new Exponent(inside, new Integer(-p)));
+			delete this;
+			return f->calculate();
+		}
+
+
+		if(typeid(*inside) == typeid(Fraction)){
+
+			Fraction* f = (Fraction*)inside;
+
+			Number* num = new Exponent(f->getNumerator(), new Integer(p));
+			Number* den = new Exponent(f->getDenominator(), new Integer(p));
+
+			Fraction* frac = new Fraction(num, den);
+
+			return frac->calculate();
+
+		}
+
+
+	}
+
+
+	if(typeid(*power) == typeid(Fraction)){
+		Fraction* f = (Fraction*)power;
+		Integer* i = (Integer*)f->getDenominator();
+
+		Exponent* exp = new Exponent(inside, f->getNumerator());
+		Root* root = new Root(exp, i->getInteger());
+
+		return root->calculate();
+
+	}
+
+	if(typeid(*inside) == typeid(Exponent)){
+		Exponent* exp = (Exponent*)inside;
+		Number** terms = new Number*[2];
+		terms[0] = power;
+		terms[1] = exp->power;
+		Multiplication* mult = new Multiplication(terms, 2);
+
+		Exponent* ans = new Exponent(exp->inside, mult);
+
+		return ans->calculate();
+	}
+
+	if(typeid(*inside) == typeid(Root)){
+		Root* root = (Root*)inside;
+
+		Exponent* exp = new Exponent(root->getInside(), power);
+
+		Root* ans = new Root(exp, root->getRoot());
+
+		return ans->calculate();
+	}
+
+	if(typeid(*inside) == typeid(Multiplication)){
+		Multiplication* mult = (Multiplication*)inside;
+
+		Number** multTerms = mult->getTerms();
+		Number** terms = new Number*[mult->getSize()];
+
+		for(int i = 0; i < mult->getSize(); i++){
+			terms[i] = new Exponent(multTerms[i], power);
+		}
+
+		Number* ans = new Multiplication(terms, mult->getSize());
+
+		return ans->calculate();
+
+	}
+
+	if(typeid(*inside) == typeid(Summation) && typeid(*power) == typeid(Integer)){
+		int p = ((Integer*)power)->getInteger();
+		Number** terms = new Number*[p];
+
+		for(int i = 0; i < p; i++){
+			terms[i] = inside;
+		}
+
+		Multiplication* ans = new Multiplication(terms, p);
+		return ans->calculate();
+	}
+
+
+	return this;
+}
+
+double Exponent::getDecimal(){
+
+	return pow(inside->getDecimal(), power->getDecimal());
+
+}
+
+string Exponent::getString(){
+	ostringstream stream;
+
+
+	if(typeid(*inside) != typeid(Integer) && typeid(*inside) != typeid(Constant)){
+		stream << "(";
+	}
+
+	stream << inside->getString();
+
+	if(typeid(*inside) != typeid(Integer) && typeid(*inside) != typeid(Constant)){
+		stream << ")";
+	}
+
+	stream << "^";
+
+	if(typeid(*power) != typeid(Integer) && typeid(*power) != typeid(Constant)){
+			stream << "(";
+		}
+
+	stream << power->getString();
+
+	if(typeid(*power) != typeid(Integer) && typeid(*power) != typeid(Constant)){
+			stream << ")";
+		}
+
+
+	return stream.str();
+
+}
+bool Exponent::equals(Number* number){
+
+	if(typeid(*number) != typeid(Exponent))
+		return false;
+
+	Exponent* e = (Exponent*)number;
+
+	Number* in1 = this->inside;
+	Number* in2 = e->inside;
+	Number* power1 = this->power;
+	Number* power2 = e->power;
+
+	return in1->equals(in2) && power1->equals(power2);
+
+}
+
+Number* Exponent::getInside(){
+	return inside;
+}
+
+Number* Exponent::getPower(){
+	return power;
+}
+
+Exponent::~Exponent(){
+	delete inside;
+	delete power;
+}
+
+
--- Fraction.cpp
+++ Fraction.cpp
@@ -1,51 +1,178 @@
-#include "Fraction.h"
-#include "Integer.h"
-#include "Constant.h"
+
 
 #include <sstream>
 #include <typeinfo>
 #include <iostream>
+#include "Fraction.h"
+#include "Integer.h"
+#include "Constant.h"
 #include <vector>
+#include "Multiplication.h"
+#include "Root.h"
+#include "Exponent.h"
 
 using namespace std;
 
-Fraction::Fraction(Number* num, Number* den) {
+Fraction::Fraction(Number* num, Number* den){
 	this->num = num;
 	this->den = den;
 }
 
-Number* Fraction::calculate() {
-	if (num->equals(den)) {
+
+Number* Fraction::calculate(){
+
+	if(num->equals(den))
 		return new Integer(1);
+
+
+	num = num->calculate();
+	den = den->calculate();
+
+
+	//Root
+
+	if(typeid(*den) == typeid(Root)){
+		Root* r = (Root*)den;
+
+		Number** terms = new Number*[2];
+		terms[0] = num;
+		terms[1] = new Exponent(r, new Integer(r->getRoot()-1));
+
+		num = new Multiplication(terms, 2);
+		den = r->getInside();
+
+		return calculate();
+
 	}
 
-	// Multiplication
-	// Rationalize denominator
-	// Reduce Summation
+
+
+	//Fractions
+
+	if(typeid(*num) == typeid(Fraction)){
+		Fraction* f = (Fraction*)num;
+		Number** numTerms = new Number*[2];
+		Number** denTerms = new Number*[2];
+
+		numTerms[0] = num;
+		numTerms[1] = f->getDenominator();
+
+		denTerms[0] = den;
+		denTerms[1] = f->getDenominator();
+
+		delete num;
+		delete den;
+
+		num = new Multiplication(numTerms, 2);
+		den = new Multiplication(denTerms, 2);
+		return calculate();
+	}
+
+
+	if(typeid(*den) == typeid(Fraction)){
+		Fraction* f = (Fraction*)den;
+
+		Number** numTerms = new Number*[2];
+		Number** denTerms = new Number*[2];
+
+		numTerms[0] = num;
+		numTerms[1] = f->getDenominator();
+
+		denTerms[0] = den;
+		denTerms[1] = f->getDenominator();
+
+		delete num;
+		delete den;
+
+		num = new Multiplication(numTerms, 2);
+		den = new Multiplication(denTerms, 2);
+		return calculate();
+	}
 
 
 
-	if (typeid(*num) == typeid(Integer) && typeid(*den) == typeid(Integer)) {
 
-		// Reduce integers
+	//Ints
+
+	if(typeid(*num) == typeid(Integer) && typeid(*den) == typeid(Integer)){
+
+		//Reduce integers
 
 		Integer* n = (Integer*) num;
 		Integer* d = (Integer*) den;
 
 		reduceInts(*n, *d);
 
-		// Delete this;
+		//delete this;
 
-		if (d->getInteger() != 1) {
+		if(d->getInteger() != 1)
 			return new Fraction(n, d);
-		}
-		else {
-			return new Integer(d->getInteger());
+		else return new Integer(n->getInteger());
+	}
+
+
+	//Multiplication
+
+
+
+	if(typeid(*num) == typeid(Multiplication) || typeid(*den) == typeid(Multiplication)){
+
+	Number** numTerms;
+	Number** denTerms;
+
+	int numSize = 1;
+	int denSize = 1;
+
+	if(typeid(*num) == typeid(Multiplication)){
+		numTerms = ((Multiplication*)num)->getTerms();
+		numSize = ((Multiplication*)num)->getSize();
+	}
+	else
+		numTerms = &num;
+
+	if(typeid(*den) == typeid(Multiplication)){
+		denTerms = ((Multiplication*)den)->getTerms();
+		denSize = ((Multiplication*)den)->getSize();
+	}
+	else
+		denTerms = &den;
+
+	for(int i = 0 ; i < numSize; i++){
+		for(int j = 0; j < denSize; j++){
+			if(numTerms[i]->equals(denTerms[j])){
+				numTerms[i] = new Integer(1);
+				denTerms[j] = new Integer(1);
+				break;
+			}
 		}
 	}
 
+	num = num->calculate();
+	den = den->calculate();
+
+	if(num->equals(den)){
+		delete this;
+		return new Integer(1);
+	}
+
+	if(typeid(*den) == typeid(Integer)){
+		if(((Integer*) den)->getInteger() == 1)
+			return num;
+	}
+
+	return this;
+	}
+
+
+
+
+
+
+
+
 	return this;
 }
+
 
 void Fraction::reduceInts(Integer& n, Integer& d){
 
@@ -54,6 +181,7 @@ void Fraction::reduceInts(Integer& n, Integer& d){
 	vector<int> dFactors = d.getFactors();
 
 	//TODO: Really hacky way of doing this
+
 
 	for(int i = 0; i < nFactors.size(); i++){
 		for(int j = 0; j < dFactors.size(); j++){
@@ -72,6 +200,7 @@ void Fraction::reduceInts(Integer& n, Integer& d){
 		numValue *= nFactors[i];
 	}
 
+
 	for(int i = 0; i < dFactors.size(); i++){
 		if(dFactors[i] != 4)
 		denValue *= dFactors[i];
@@ -82,6 +211,9 @@ void Fraction::reduceInts(Integer& n, Integer& d){
 
 }
 
+
+
+
 Number* Fraction::getNumerator(){
 	return num;
 }
@@ -90,46 +222,46 @@ Number* Fraction::getDenominator(){
 	return den;
 }
 
+
 double Fraction::getDecimal(){
 	return num->getDecimal() / den->getDecimal();
 }
 
+
 string Fraction::getString(){
 	ostringstream stream;
+
+
+
 
 	string s = typeid(*num).name();
 	string s1 = typeid(*den).name();
 
-	if ((s != typeid(Integer).name()) && s != typeid(Constant).name()) {
+	if((s != typeid(Integer).name()) && s != typeid(Constant).name())
 		stream << "(";
-	}
 
 	stream << num->getString();
 
-	if ((s != typeid(Integer).name()) && s != typeid(Constant).name()) {
+	if((s != typeid(Integer).name()) && s != typeid(Constant).name())
 			stream << ")";
-	}
 
 	stream << "/";
 
-	if ((s1 != typeid(Integer).name()) && s1 != typeid(Constant).name()) {
+	if((s1 != typeid(Integer).name()) && s1 != typeid(Constant).name())
 			stream << "(";
-	}
 
 	stream << den->getString();
 
-	if ((s1 != typeid(Integer).name()) && s1 != typeid(Constant).name()) {
-			stream << ")";
-	}
+	if((s1 != typeid(Integer).name()) && s1 != typeid(Constant).name())
+				stream << ")";
 
 	return stream.str();
 }
 
-bool Fraction::equals(Number* number){
-	if (typeid(*number) != typeid(Fraction)) {
-		return false;
-	}
 
+bool Fraction::equals(Number* number){
+	if(typeid(*number) != typeid(Fraction))
+		return false;
 	Fraction* f = (Fraction*)number;
 
 	Number* num1 = this->num;
@@ -140,6 +272,8 @@ bool Fraction::equals(Number* number){
 	return num1->equals(num2) && den1->equals(den2);
 
 }
+
+
 
 Fraction::~Fraction(){
 	delete num;
